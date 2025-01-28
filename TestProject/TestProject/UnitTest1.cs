4646896b5b4b43e626169bc19fe599ad2c4f51b6
Nataniel Farzan <natanielfarzan@gmail.com>
2025-01-27 21:33:45 -0800

    Add cooler feature

--- TestProject/TestProject/UnitTest1.cs
+++ TestProject/TestProject/UnitTest1.cs
@@ -9,8 +9,9 @@ public class UnitTest1
     }
 
     [Fact]
-    public void Test2()
+    public void FalseIsFalseAndStringIsEmpty()
     {
         Assert.False(!true);
+        Assert.Empty("");
     }
 }
